867262fb7f89e6b4d84e99e356b8397d9ac9910c
Muhammad Suleman <m.suleman996@gmail.com>
2021-11-15 02:33:19 +0500

    Init

---
+++ primary-category.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Primary Category
+ * Description: Add primary Category to post, page, or CPT.
+ * Version: 1.0.0
+ * Author: Suleman
+ * Author URI: https://suleman-help.me
+ * Text Domain: primary-category
+ *
+ */
+
+namespace Suleman\PrimaryCategory;
+
+// site-level autoloading.
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require( 'path' );
+
+/**
+ * WordPress dependencies
+ */
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+module.exports = {
+	...defaultConfig,
+	entry: {
+		editor: './assets/js/src/block-editor.js',
+	},
+	output: {
+		path: path.resolve( __dirname, 'js/dist' ),
+		filename: '[name].js',
+	},
+};
